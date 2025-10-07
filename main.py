2b7dda721bb4a4c892cac6fbef6a0d6bc64b0455
vitovillani <villanivito1@gmail.com>
2025-10-07 13:00:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+import openai
+
+import feedparser
+
+from telegram import Update
+
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+
+
